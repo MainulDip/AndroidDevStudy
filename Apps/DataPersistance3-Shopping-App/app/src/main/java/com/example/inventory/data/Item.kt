7f766001f05c83ce332ba58d88a8763ddca3ff3f
MainulDip <mainuldip@gmail.com>
2023-02-26 00:30:05 -0500

    android datapersistance viewmodel init

--- Apps/DataPersistance3-Shopping-App/app/src/main/java/com/example/inventory/data/Item.kt
+++ Apps/DataPersistance3-Shopping-App/app/src/main/java/com/example/inventory/data/Item.kt
@@ -5,7 +5,9 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "item")
-data class Item (@PrimaryKey(true) val id: Int = 0,
-                 @ColumnInfo("name") val itemName: String,
-                 @ColumnInfo("price") val itemPrice: Double,
-                 @ColumnInfo("quantity") val quantityInStock: Int)
+data class Item(
+    @PrimaryKey(true) val id: Int = 0,
+    @ColumnInfo("name") val itemName: String,
+    @ColumnInfo("price") val itemPrice: Double,
+    @ColumnInfo("quantity") val quantityInStock: Int
+)
