dae7000c2566c0dd056b0879c4881dce4ca655d5
MainulDip <mainuldip@gmail.com>
2023-02-26 20:03:41 -0500

    android adding item into SQLlite database using room and inspecting using app-inspection android studio

--- Apps/DataPersistance3-Shopping-App/app/src/main/java/com/example/inventory/AddItemFragment.kt
+++ Apps/DataPersistance3-Shopping-App/app/src/main/java/com/example/inventory/AddItemFragment.kt
@@ -23,7 +23,9 @@ import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.inventory.data.Item
 import com.example.inventory.databinding.FragmentAddItemBinding
 
 /**
@@ -37,6 +39,8 @@ class AddItemFragment : Fragment() {
             (activity?.application as InventoryApplication).database.itemDao()
         )
     }
+
+    lateinit var item: Item
 
     private val navigationArgs: ItemDetailFragmentArgs by navArgs()
 
@@ -53,6 +57,33 @@ class AddItemFragment : Fragment() {
     ): View? {
         _binding = FragmentAddItemBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    private fun isEntryValid(): Boolean {
+        return viewModel.isEntryValid(
+            binding.itemName.text.toString(),
+            binding.itemPrice.text.toString(),
+            binding.itemCount.text.toString()
+        )
+    }
+
+    private fun addNewItem() {
+        if (isEntryValid()) {
+            viewModel.addNewItem(
+                binding.itemName.text.toString(),
+                binding.itemPrice.text.toString(),
+                binding.itemCount.text.toString(),
+            )
+        }
+        val action = AddItemFragmentDirections.actionAddItemFragmentToItemListFragment()
+        findNavController().navigate(action)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.saveAction.setOnClickListener {
+            addNewItem()
+        }
     }
 
     /**
