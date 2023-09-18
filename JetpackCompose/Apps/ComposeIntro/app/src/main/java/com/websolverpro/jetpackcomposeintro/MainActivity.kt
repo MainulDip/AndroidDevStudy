1bb7649e368b8025f69f31cb9bb4445cdce998e8
mainuldip <mainuldip@gmail.com>
2023-09-17 20:50:49 -0400

    android-study JetPack into app with image and centered text

--- JetpackCompose/Apps/ComposeIntro/app/src/main/java/com/websolverpro/jetpackcomposeintro/MainActivity.kt
+++ JetpackCompose/Apps/ComposeIntro/app/src/main/java/com/websolverpro/jetpackcomposeintro/MainActivity.kt
@@ -3,45 +3,73 @@ package com.websolverpro.jetpackcomposeintro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.websolverpro.jetpackcomposeintro.ui.theme.JetpackComposeIntroTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            JetpackComposeIntroTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    Greeting("Android Again")
-//                }
-//            }
-            Greeting("Android Again")
+            JetpackComposeIntroTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android Again")
+                }
+            }
         }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Box (contentAlignment = Alignment.Center){
+        Image(painter = painterResource(id = R.drawable.pirate), contentDescription = "A Pirate", contentScale = ContentScale.FillBounds ,modifier = Modifier.fillMaxSize())
+        Box(modifier = Modifier
+            .background(color = Color.Red.copy(alpha = 0.7F), shape = RoundedCornerShape(50))
+            .padding(20.dp, 20.dp)
+        ) {
+            Text(
+                text = "Hello $name!",
+                modifier = modifier,
+                color = Color.White
+            )
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackComposeIntroTheme {
-        Greeting("Android")
+        Greeting("Android Again")
     }
 }
--- JetpackCompose/Compose-1.md
+++ JetpackCompose/Compose-1.md
@@ -5,4 +5,57 @@ Modern toolkit for building native Android UI with declarative functions using K
 * MainActivity: Starting point, which extends the ComponentActivity
 * setContent | ComponentActivity.setContent : Creates the root view of the UI. 
 * Composable function:
-@Composable annotation makes it into a composable function, it's the building block of Jetpack Compose. It describe the UI/Views and provide data dependencies. Any function including a @Composable function needs to be itself @Composable
+@Composable annotation makes it into a composable function, it's the building block of JetPack Compose. It describe the UI/Views and provide data dependencies. Any function including a @Composable function needs to be itself @Composable
+* Theme:
+All the theming (Material) files are inside ui.theme (on default compose template). The main theme function is a composable. And there is a @Composable "Surface" from Material 3, usually all other components go inside this.
+* @Preview:
+@Preview annotation can display the composable functions call without building the project and running on an emulator.
+
+```kotlin
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeIntroTheme { // Composable Theme Function, Which call Material 3 
+                Surface( // also a Material Theme's Composable Function
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android Again")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Box (contentAlignment = Alignment.Center){
+        Image(painter = painterResource(id = R.drawable.pirate), contentDescription = "A Pirate", contentScale = ContentScale.FillBounds ,modifier = Modifier.fillMaxSize())
+        Box(modifier = Modifier
+            .background(color = Color.Red.copy(alpha = 0.7F), shape = RoundedCornerShape(50))
+            .padding(20.dp, 20.dp)
+        ) {
+            Text(
+                text = "Hello $name!",
+                modifier = modifier,
+                color = Color.White
+            )
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeIntroTheme {
+        Greeting("Android Again")
+    }
+}
+```
+### Layout Basic:
+- Column : arrange elements vertically
+- Row : arrange items horizontally
+- Box : For Stacking Components
+- Spacer : provide horizontal spacing
