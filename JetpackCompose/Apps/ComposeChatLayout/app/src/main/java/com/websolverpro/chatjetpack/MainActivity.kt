7ceed9cebc60b008e0a5edf8d93aaf0a1a3a2fe4
mainuldip <mainuldip@gmail.com>
2023-10-19 21:24:18 -0400

    android-study jetpack compose chat application ui

--- JetpackCompose/Apps/ComposeChatLayout/app/src/main/java/com/websolverpro/chatjetpack/MainActivity.kt
+++ JetpackCompose/Apps/ComposeChatLayout/app/src/main/java/com/websolverpro/chatjetpack/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -21,6 +24,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -40,8 +47,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MessageCard(Message("Android", "Jetpack Compose"))
-
+                    Conversation(SampleData.conversationSample)
                 }
             }
         }
@@ -59,7 +65,9 @@ fun MessageCard(msg: Message) {
             painter = painterResource(R.drawable.profile_picture),
             contentDescription = "Contact profile picture",
             contentScale = ContentScale.Crop,
-            modifier = Modifier.size(40.dp).padding(end = 10.dp)
+            modifier = Modifier
+                .size(40.dp)
+                .padding(end = 10.dp)
                 .clip(CircleShape)
                 .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
 
@@ -67,16 +75,27 @@ fun MessageCard(msg: Message) {
 
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        // state, keep track if message is expanded or not
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+
+        val surfaceColor by animateColorAsState(if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+            label = "text animation"
+        )
+
+        Column (modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(text = msg.author, color = MaterialTheme.colorScheme.secondary,
                 style = MaterialTheme.typography.titleSmall)
 
             Spacer(modifier = Modifier.height(4.dp))
 
-            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp,
+                color = surfaceColor, modifier = Modifier.animateContentSize().padding(1.dp)) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.bodyMedium
                 )
             }
