c37544acb1569a46f84910b3e1a484ab22c6ac32
mainuldip <mainuldip@gmail.com>
2023-12-03 21:25:59 -0500

    android-study setting app for testing Service App Components

--- WorkManager-Service-BroadCast/Apps/Service-1/app/src/main/java/com/websolverpro/service_1/MainActivity.kt
+++ WorkManager-Service-BroadCast/Apps/Service-1/app/src/main/java/com/websolverpro/service_1/MainActivity.kt
@@ -2,10 +2,30 @@ package com.websolverpro.service_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import com.websolverpro.service_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.apply {
+            btnStart.setOnClickListener {
+                Log.d("First Button Call", "onCreate: Start btn clicked")
+            }
+
+            btnEnd.setOnClickListener {
+                Log.d("Second Button Call", "onCreate: End btn clicked")
+            }
+        }
+
     }
 }
