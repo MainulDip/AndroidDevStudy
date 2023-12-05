b9a51fadb2f17ad0e48126289559ded737126f47
mainuldip <mainuldip@gmail.com>
2023-12-05 04:20:46 -0500

    android-study minimal implementation of RxJava and RxAndroid App

--- Coroutine-and-Reactive-Programming/Apps/RxAndroid-1/app/src/main/java/com/websolverpro/rxandroid_1/MainActivity.java
+++ Coroutine-and-Reactive-Programming/Apps/RxAndroid-1/app/src/main/java/com/websolverpro/rxandroid_1/MainActivity.java
@@ -3,15 +3,54 @@ package com.websolverpro.rxandroid_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final static String API = "RxAndroidCustom";
+    private final static String TAG = "RxAndroidCustom";
     private String greating = "Hello from RxJava and RxAndroid";
+    private Observable<String> myObservable;
+    private Observer<String> myObserver;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.tvGreeting);
+
+        myObservable = Observable.just(greating);
+
+        myObserver = new Observer<String>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.d(TAG, "onSubscribe: ");
+            }
+
+            @Override
+            public void onNext(@NonNull String s) {
+                Log.d(TAG, "onNext: ");
+                textView.setText("Observer Text");
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.d(TAG, "onError: ");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: ");
+            }
+        };
+
+        myObservable.subscribe(myObserver);
     }
 }
